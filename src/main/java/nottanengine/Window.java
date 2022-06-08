04cffa997fc49fee1717095f0c5793f539fe54c3
Deepesh Nimma <deepnimma@gmail.com>
2022-06-08 22:02:50 +0530

    Event listeners

--- src/main/java/nottanengine/Window.java
+++ src/main/java/nottanengine/Window.java
@@ -4,6 +4,7 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
@@ -25,6 +26,7 @@ public class Window {
     public static Window get() {
         // If the window does not exist, it creates one.
         if (Window.window == null) {
+            System.out.println("Creating a new Window.");
             Window.window = new Window();
         }
 
@@ -36,6 +38,16 @@ public class Window {
 
         init();
         loop();
+
+        System.out.println("Freeing memory.");
+        // Free Memory once the loop has exited.
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        System.out.println("Terminating GLFW and freeing the error callbacks.");
+        // Terminate GLFW and free the error callbacks.
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     public void init() {
@@ -60,6 +72,17 @@ public class Window {
         if (glfwWindow == NULL) {
             throw new IllegalStateException("Failed to create the GLFW window.");
         }
+
+        // Setup Callbacks.
+        // :: means lambda functions.
+
+        // Mouse Callbacks.
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+
+        // Key Callbacks
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
 
         // Make the OpenGL context current.
         glfwMakeContextCurrent(glfwWindow);
